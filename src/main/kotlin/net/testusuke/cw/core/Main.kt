eacba071187763b6f0c28f6dc42059dff6b1e12b
owner <forgotten_saga_1129@outlook.jp>
2020-07-28 07:02:22 +0900

    [debug]readmeの検証-2

--- src/main/kotlin/net/testusuke/cw/core/Main.kt
+++ src/main/kotlin/net/testusuke/cw/core/Main.kt
@@ -1,8 +1,22 @@
 package net.testusuke.cw.core
 
+import org.bukkit.plugin.java.JavaPlugin
+
 /**
  * Created by testusuke on 2020/07/27
  * @author testusuke
  */
-class Main {
+class Main:JavaPlugin() {
+
+    companion object{
+        lateinit var plugin: Main
+    }
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
 }
